20fe20b84d182a5b4e6d69270412d5857b1ed8d8
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-10-15 21:48:11 +0300

    install dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+import path from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
   server: {
     open: './index.html'
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
