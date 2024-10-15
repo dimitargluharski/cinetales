a7766ad3ae80d99a53c7966ad7e64c91db0eeabe
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-10-15 22:20:42 +0300

    remove comment

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
 
-import './assets/index.css' // Your custom CSS if needed
+import './assets/index.css'
 
 const app = createApp(App)
 app.use(BootstrapVue3)
