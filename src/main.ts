18c5b86912225af145f6de8b312643a1747467bc
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-10-15 22:21:22 +0300

    remove white space

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 // @ts-ignore
 import BootstrapVue3 from 'bootstrap-vue-3'
-
-
 import './assets/index.css'
 
 const app = createApp(App)
